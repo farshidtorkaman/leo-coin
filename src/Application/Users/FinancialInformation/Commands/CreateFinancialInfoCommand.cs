2a1ce81df1a1aeed25a6c2bb0fc58d56a553c889
Farshid Torkaman <farshidtorkaman@outlook.com>
2020-06-19 22:46:47 +0430

    price calculation and some other features

---
+++ src/Application/Users/FinancialInformation/Commands/CreateFinancialInfoCommand.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Crypto.Application.Common.Interfaces;
+using Crypto.Domain.Entities;
+using Crypto.Domain.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crypto.Application.Users.FinancialInformation.Commands
+{
+    public class CreateFinancialInfoCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public string CardNumber { get; set; }
+        public int? BankId { get; set; }
+        public string AccountNumber { get; set; }
+        public string Sheba { get; set; }
+        public IFormFile BankCardImage { get; set; }
+    }
+
+    public class CreateFinancialInfoCommandHandler : IRequestHandler<CreateFinancialInfoCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IImageAccessor _imageAccessor;
+        private readonly ICurrentUserService _currentUserService;
+
+        public CreateFinancialInfoCommandHandler(IApplicationDbContext context, IImageAccessor imageAccessor,
+            ICurrentUserService currentUserService)
+        {
+            _context = context;
+            _imageAccessor = imageAccessor;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<int> Handle(CreateFinancialInfoCommand request, CancellationToken cancellationToken)
+        {
+            var financialInformationCount = await _context.FinancialInformation
+                .Where(f => f.CreatedBy == _currentUserService.UserId).CountAsync(cancellationToken);
+            
+            var financialInfo = new FinancialInfo
+            {
+                CardNumber = request.CardNumber,
+                BankId = request.BankId == 0 ? null : request.BankId,
+                AccountNumber = request.AccountNumber,
+                Sheba = request.Sheba,
+                BankCardImage = await _imageAccessor.Upload(request.BankCardImage, _currentUserService.UserId,
+                    "کارت بانکی " + ++financialInformationCount),
+                Status = Status.Sent
+            };
+            _context.FinancialInformation.Add(financialInfo);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return financialInfo.Id;
+        }
+    }
+}
---
+++ src/Application/Users/FinancialInformation/Commands/CreateFinancialInfoCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Crypto.Application.Users.FinancialInformation.Commands
+{
+    public class UpdateFinancialInfoCommandValidator : AbstractValidator<UpdateFinancialInfoCommand>
+    {
+        public UpdateFinancialInfoCommandValidator()
+        {
+            RuleFor(f => f.CardNumber)
+                .NotEmpty().WithMessage("شماره کارت خود را وارد کنید")
+                .Length(16).WithMessage("شماره کارت معتبر وارد نمایید");
+            RuleFor(f => f.AccountNumber)
+                .NotEmpty().WithMessage("شماره حساب خود را وارد کنید");
+            RuleFor(f => f.BankId)
+                .NotEmpty().WithMessage("بانک خود را انتخاب نمایید");
+            RuleFor(f => f.Sheba)
+                .NotEmpty().WithMessage("شماره شبای خود را وارد کنید")
+                .Length(24).WithMessage("شماره شبای معتبر وارد نمایید");
+            RuleFor(f => f.BankCardImage)
+                .NotEmpty().WithMessage("عکس کارت بانکی خود را بارگذاری نمایید");
+        }
+    }
+}
